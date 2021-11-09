8a116f96c8ac1f2134bae3b0db0b4802accc1c24
Prath <prathmesh@probosys.com>
2021-11-09 21:01:04 +0530

    My application host

--- app.js
+++ app.js
@@ -1,23 +1,8 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
-
-let bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.listen(port, () => console.log("port 3000 !"));
-
- var response;
-
-app.post('/process_cake_order',function(req,res){
-   
-	
-    res.json();
-	
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
-
-app.get("/process_get", (req, res) => res.json(response));
-
-
-
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
